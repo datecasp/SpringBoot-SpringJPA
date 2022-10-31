cc805284f47cace5ecdc94c818420fe727ea1c40
datecasp <ciudadano.fake@gmail.com>
2022-10-31 17:46:16 +0100

    Create Curso (Entity, interface an dcontroller). Update Seeder. Create NivelCurso enum

--- src/main/java/com/datecasp/SpringBootSpringJPA/Exceptions/AlumnoNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package com.datecasp.SpringBootSpringJPA.exceptions;
-
-public class AlumnoNotFoundException extends RuntimeException {
-
-    public AlumnoNotFoundException(Long id) {
-        super("Could not find employee " + id);
-    }
-}
