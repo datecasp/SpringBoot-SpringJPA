fc472f6d93f638b5ba065f6bd14f63cf622101f9
datecasp <ciudadano.fake@gmail.com>
2022-11-03 23:18:43 +0100

    No funciona rol ADMIN

--- src/main/java/com/datecasp/SpringBootSpringJPA/Controllers/AlumnoController.java
+++ src/main/java/com/datecasp/SpringBootSpringJPA/Controllers/AlumnoController.java
@@ -14,6 +14,7 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
+@RequestMapping("api/v1/")
 public class AlumnoController
 {
     //Atributo para tener acceso al contexto
--- src/main/java/com/datecasp/SpringBootSpringJPA/helpers/WebSecurityConfig.java
+++ src/main/java/com/datecasp/SpringBootSpringJPA/helpers/WebSecurityConfig.java
@@ -2,16 +2,21 @@ package com.datecasp.SpringBootSpringJPA.helpers;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.parameters.P;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 @EnableWebSecurity
@@ -20,10 +25,9 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter
     @Override
     protected void configure(HttpSecurity http) throws Exception
     {
-        //Patrón de api para permintir sin login antMatchers("")
         http.authorizeRequests()
-                .antMatchers("User").hasAnyRole("ADMIN", "USER")
-                .antMatchers("Admin").hasRole("ADMIN")
+                .antMatchers("/User").hasAnyRole("USER","ADMIN")
+                .antMatchers("/Admin").hasRole("ADMIN")
                 .anyRequest().authenticated()
                 .and()
                 .formLogin()
@@ -40,8 +44,29 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter
                 .passwordEncoder(passwordEncoder())
                 .withUser("usuario").password(passwordEncoder().encode("1234")).roles("USER")
                 .and()
-                .withUser("admin").password(passwordEncoder().encode("1234")).roles("ADMIN", "USER");
+                .withUser("jefe").password(passwordEncoder().encode("1234")).roles("ADMIN");
     }
+
+
+//    @Bean
+//    @Override
+//    protected UserDetailsService userDetailsService() {
+//        UserDetails usuario = User.builder()
+//                .username("usuario")
+//                .password(passwordEncoder().encode("1234"))
+//                .roles("USER")
+//                .build();
+//        UserDetails jefe = User.builder()
+//                .username("jefe")
+//                .password(passwordEncoder().encode("1234"))
+//                .roles("ADMIN")
+//                .build();
+//        InMemoryUserDetailsManager userDetailsManager = new InMemoryUserDetailsManager(usuario, jefe);
+//
+//        return userDetailsManager;
+//
+//    }
+
 
     //Codificador de password
     @Bean
